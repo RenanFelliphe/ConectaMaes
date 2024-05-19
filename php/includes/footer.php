0dfd5479f5f1ec3c7f5ea85a3ec8bb4932d39229
Renan Felliphe <felliphemouraa@gmail.com>
2024-05-19 16:02:30 -0300

    Box-shadows feature

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -1,34 +1,46 @@
-<footer>
-    <div class="A-footerLeft">
-        <div class="A-footerContact">
-            <h4>Contato</h4>
-            <div class="A-footerSocialMedias">
-                <i class="bi bi-instagram"></i>
-                <i class="bi bi-facebook"></i>
-                <i class="bi bi-envelope-fill"></i>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="styles/variable.css">
+        <link rel="stylesheet" href="styles/include.css">
+    </head>
+    <body>
+        <footer>
+            <div class="A-footerLeft">
+                <div class="A-footerContact">
+                    <h4>Contato</h4>
+                    <div class="A-footerSocialMedias">
+                        <i class="bi bi-instagram"></i>
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-envelope-fill"></i>
+                    </div>
+                </div>    
             </div>
-        </div>    
-    </div>
-    
-    <span class="A-copyrightSign">
-        <i class="bi bi-c-circle"></i>
-        <p>2024 ConectaMães do CEFET-MG</p>
-    </span>
+            
+            <span class="A-copyrightSign">
+                <i class="bi bi-c-circle"></i>
+                <p>2024 ConectaMães do CEFET-MG</p>
+            </span>
 
-    <ul class="A-footerRight">
-        <li class="A-footerAbout">
-            <h5>Sobre</h5>
-            <p class="A-footerAboutProject">Projeto</p>
-            <p class="A-footerAboutEquip">CEFET-MG</p>
-        </li>
-        <li class="A-footerHelp">
-            <h5>Ajuda</h5>
-            <p class="A-footerHelpSuport">Suporte</p>
-        </li>
-        <li class="A-footerPolitic">
-            <h5>Política</h5>
-            <p class="A-footerPoliticPrivacy">Privacidade</p>
-            <p class="A-footerPoliticTerms">Termos de Uso</p>
-        </li>
-    </ul>
-</footer>
+            <ul class="A-footerRight">
+                <li class="A-footerAbout">
+                    <h5>Sobre</h5>
+                    <p class="A-footerAboutProject">Projeto</p>
+                    <p class="A-footerAboutEquip">CEFET-MG</p>
+                </li>
+                <li class="A-footerHelp">
+                    <h5>Ajuda</h5>
+                    <p class="A-footerHelpSuport">Suporte</p>
+                </li>
+                <li class="A-footerPolitic">
+                    <h5>Política</h5>
+                    <p class="A-footerPoliticPrivacy">Privacidade</p>
+                    <p class="A-footerPoliticTerms">Termos de Uso</p>
+                </li>
+            </ul>
+        </footer>
+    </body>
+</html>
--- php/includes/header.php
+++ php/includes/header.php
@@ -1,16 +1,28 @@
-<header>
-    <a href="index.php"><img src="assets/Logos/Final/Conecta_Mães_Logo_Black.png" class="A-headerLogo" alt="Logo do ConectaMães"></a>
-    <div class="A-headerLinks">
-        <h4>Equipe</h4>
-        <h4>Contato</h4>
-        <h4>Direitos</h4>
-    </div>
-    <div class="A-searchBar">
-        <i class="bi bi-search"></i>
-        <input type="search" class="A-searchBarInput" placeholder="Pesquisar">
-    </div>
-    <div class="A-signButtons">
-        <a href="register.php"><h4 class="A-signUp">Cadastrar</h4></a>
-        <a href="login.php"><h4 class="A-signIn">Login</h4></a>
-    </div>
-</header>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="styles/variable.css">
+        <link rel="stylesheet" href="styles/include.css">
+    </head>
+    <body>
+        <header>
+            <a href="index.php"><img src="assets/Logos/Final/Conecta_Mães_Logo_Black.png" class="A-headerLogo" alt="Logo do ConectaMães"></a>
+            <div class="A-headerLinks">
+                <h4>Equipe</h4>
+                <h4>Contato</h4>
+                <h4>Direitos</h4>
+            </div>
+            <div class="A-searchBar">
+                <i class="bi bi-search"></i>
+                <input type="search" class="A-searchBarInput" placeholder="Pesquisar">
+            </div>
+            <div class="A-signButtons">
+                <a href="register.php"><h4 class="A-signUp">Cadastrar</h4></a>
+                <a href="login.php"><h4 class="A-signIn">Login</h4></a>
+            </div>
+        </header>
+    </body>
+</html>
