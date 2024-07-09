0e69e7cbe1aff12232ac450a56f439849db7c248
lialess1808 <nathaliacamposlessa1@gmail.com>
2024-07-09 14:45:09 -0300

    add db connection helper

--- app/services/auth/authUser.php
+++ app/services/auth/authUser.php
@@ -1,10 +1,5 @@
 <?php
-    $hostname = '162.240.17.101';
-    $username = 'projetos_nlessa';
-    $password = 'Gc&sgY74PK$}';
-    $database = 'projetos_INF2023_G10';
-
-    $conn = mysqli_connect($hostname, $username, $password, $database);
+    include_once(__DIR__ .'/../helpers/conn.php');
 
     // LOG IN AND OUT FUNCTIONS
     function logIn($conn){
