2de280a5e8300a4de9e9b6ff36d1af168cf9a94d
Renan Felliphe <felliphemouraa@gmail.com>
2024-10-25 19:53:01 -0300

    Suport Page Layout and Animations Correction

--- public/suporte.php
+++ public/suporte.php
@@ -25,44 +25,44 @@
             <section class="Su-cellsBackground">
                 <h1>Suporte</h1>
                 <div class="Su-searchbar">
-                    <input type="text" placeholder="Pesquisar">
+                    <label class="bi bi-search" for="Su-searchBarInput"></label>
+                    <input type="search" placeholder="Pesquisar" id="Su-searchBarInput">
                 </div>
-                <img src="../app/assets/imagens/figuras/suportBack.png" alt="" class="Su-background">
             </section>
 
             <h2 class="La-sectionTitle">Precisa de ajuda?</h2>
 
             <section class="Su-helpSection">
                 <article class="Su-articleCards">
-                    <div class="Su-card">
+                    <a href="" class="Su-card">
                         <img src="../app/assets/imagens/icons/Su_novidades.png" alt="" class="Su-cardIcons">
                         <h3>Novidades</h3>
                         <p>Fique por dentro das últimas <span class="Su-focusyellow">notícias </span> do ConectaMães</p>
-                    </div>
+                    </a>
 
-                    <div class="Su-card">
+                    <a href="" class="Su-card">
                         <img src="../app/assets/imagens/icons/Su_interface.png" alt="" class="Su-cardIcons">
                         <h3>Interface</h3>
                         <p><span class="Su-focusblue">O que esse botão faz?</span> Entenda tudo sobre a interface do sistema</p>
-                    </div>
+                    </a>
 
-                    <div class="Su-card">
+                    <a href="" class="Su-card">
                         <img src="../app/assets/imagens/icons/Su_config.png" alt="" class="Su-cardIcons">
                         <h3>Configurações</h3>
                         <p><span class="Su-focuspink">Personalize</span> o sistema como for melhor para você</p>
-                    </div>
+                    </a>
 
-                    <div class="Su-card">
+                    <a href="" class="Su-card">
                         <img src="../app/assets/imagens/icons/Su_politicas.png" alt="" class="Su-cardIcons">
                         <h3>Políticas</h3>
                         <p>Mantendo as coisas<span class="Su-focusyellow"> seguras </span>para todos</p>
-                    </div>
+                    </a>    
 
-                    <div class="Su-card">
+                    <a href="" class="Su-card">
                         <img src="../app/assets/imagens/icons/Su_faq.png" alt="" class="Su-cardIcons">
                         <h3>FAQ</h3>
                         <p>Veja se já responderam sua <span class="Su-focusblue">dúvida</span></p>
-                    </div>
+                    </a>
                 </article>
             </section>
         </main>
